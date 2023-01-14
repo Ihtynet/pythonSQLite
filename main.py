87f87b5f00563bebc3c27fff68751b1df21b0075
Владимир <50101573+Ihtynet@users.noreply.github.com>
2023-01-15 02:06:56 +0300

    123

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import sqlite3
 conn = sqlite3.connect("org1.db")
 cursor = conn.cursor()
 
+# Создание таблицы tovary
+#
+cursor.execute("""CREATE TABLE tovary
+                 (code,name,country,originalname,manufacturer)
+               """)
 
-"""fo =  open("org1/tovary.csv", "r", encoding="utf-8")
+cursor.execute("""CREATE TABLE sales
+                  (code,client,kolichestvo,summa,nomer,date)
+               """)
+
+cursor.execute("""CREATE TABLE zakupki
+                  (code,client,kolichestvo,summa,nomer,date)
+               """)
+conn.commit()
+
+fo =  open("org1/tovary.csv", "r", encoding="utf-8")
 mas = fo.read().split("\n")
 for  s in mas:
     if s == "":
@@ -22,18 +36,18 @@ fo =  open("org1/sales.csv", "r", encoding="utf-8")
 mas = fo.read().split("\n")
 for  s in mas:
     masS = s.split("::")
-    tekStroka = (masS[0], masS[1],masS[2],masS[3],masS[4])
-    cursor.execute("INSERT INTO sales VALUES(?, ?, ?, ?, ?);", tekStroka)
+    tekStroka = (masS[0], masS[1],masS[2],masS[3],masS[4],masS[5])
+    cursor.execute("INSERT INTO sales VALUES(?, ?, ?, ?, ?, ?);", tekStroka)
     conn.commit()
-fo.close()"""
+fo.close()
 
 fo =  open("org1/zakupki.csv", "r", encoding="utf-8")
 mas = fo.read().split("\n")
 for  s in mas:
     masS = s.split("::")
     #print(masS)
-    tekStroka = (masS[0], masS[1],masS[2],masS[3],masS[4])
-    cursor.execute("INSERT INTO zakupki VALUES(?, ?, ?, ?, ?);", tekStroka)
+    tekStroka = (masS[0], masS[1],masS[2],masS[3],masS[4],masS[5])
+    cursor.execute("INSERT INTO zakupki VALUES(?, ?, ?, ?, ?, ?);", tekStroka)
     conn.commit()
 fo.close()
 
